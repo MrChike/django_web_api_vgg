469dbbc56e7ff6077aed5580c433567d56655563
MrChike <chikeegonu@gmail.com>
2020-02-09 07:35:09 +0100

    commited for deployment

--- web_api_project/settings.py
+++ web_api_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vwi7o^h@*fpq)%f_y2l!gb!n_uk+6^u8%%$=ha8yn)ad#ssz@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'web_api_app.Users'
+
+STATIC_ROOT = 'static/'
